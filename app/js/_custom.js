b9bfabb97aac21d5e390a201397d249158a5bbc4
Vitalii Dubrovets <vitdub@inbox.ru>
2019-08-03 17:57:15 +0600

    added portfolio

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -7,4 +7,43 @@ document.addEventListener("DOMContentLoaded", function() {
 			$('body,html').animate({ scrollTop: top }, 1500);
 		});
 	});
+	function removeClass(elem, clas){
+		elem.classList.remove(clas);
+	}
+	function addClass(elem, clas){
+		elem.classList.add(clas);
+		console.log()
+	}
+	function isActive(elem) {
+		var result = false;
+		var classList = elem.classList;
+		var targetClass = 'btn-portfolio-active';
+		for(var i = 0; i < classList.length; i++){
+			if (classList[i] == targetClass){
+				result = true;
+			}
+		}
+		//console.log('isActive - ' + classList + ' ' + result);
+		return result;
+	}
+	function findActive(){
+		var index = -1;
+		$('.btn-portfolio').each(function (i){
+			if (isActive($('.btn-portfolio')[i])){
+				index = i;
+			}
+		});
+		return index;
+	}
+	var curr = 0;
+	$('.btn-portfolio').on('click', function(){
+
+		removeClass($('.btn-portfolio')[curr], 'btn-portfolio-active');
+		addClass(this, 'btn-portfolio-active');
+
+		removeClass($('.portfolio-item')[curr], 'portfolio-item-active');
+		curr = findActive();
+		addClass($('.portfolio-item')[curr], 'portfolio-item-active');
+	});
+	//console.log($('.btn-portfolio')[curr].classList);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,8 @@ gulp.task('scripts', function() {
 	return gulp.src([
 		'node_modules/jquery/dist/jquery.min.js', // Optional jQuery plug-in (npm i --save-dev jquery)
 		'app/js/_lazy.js', // JS library plug-in example
+		'app/libs/fotorama/fotorama.js',
+		'app/libs/owl.carousel/dist/owl.carousel.min.js',
 		'app/js/_custom.js', // Custom scripts. Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
